1f1dae0ac884b1ffb2ff6cfb32c156bce6579b70
Roman Zvir <roman.zvir.pp.2023@lpnu.ua>
2025-05-29 12:50:43 +0300

    Update os-info.sh

--- os-info.sh
+++ os-info.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-echo "Версія ОС:"
+echo "OS version:"
 cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'
 echo
 
-echo "Користувачі з оболонкою bash:"
-# Виводимо користувачів, у яких оболонка закінчується на /bash
+echo "Users with bash:"
 grep '/bash$' /etc/passwd | cut -d ':' -f1
 echo
 
-echo "Відкриті порти на машині:"
-# Виводимо всі відкриті порти (TCP і UDP)
-# Потрібен sudo або права root для повного списку
+echo "Open ports:"
 sudo ss -tuln
 
