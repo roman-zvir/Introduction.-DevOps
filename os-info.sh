ba8e1c68973aaa577fd9504d737a406e395c363b
Roman Zvir <roman.zvir.pp.2023@lpnu.ua>
2025-05-29 13:03:39 +0300

    Update os-info.sh

--- os-info.sh
+++ os-info.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-echo "OS version:"
-cat /etc/os-release | grep PRETTY_NAME | cut -d '=' -f2 | tr -d '"'
-echo
+echo -e "OS version:\n$(uname -a)\n"
 
 echo "Users with bash:"
 grep '/bash$' /etc/passwd | cut -d ':' -f1
